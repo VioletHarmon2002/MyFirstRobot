8c595830f675de71cab3b16a24d8acb19d9fb8c8
Jayden van Oorschot <jayden.van.oorschot@hva.nl>
2024-05-13 13:48:17 +0200

    Created basic walking animation

--- embedded/robotB/walkingTest/walkingTest.ino
+++ embedded/robotB/walkingTest/walkingTest.ino
@@ -1,52 +1,56 @@
 #include <ESP32Servo.h>
 
 // Define the pins for the servos
-#define SERVO1_PIN 1
-#define SERVO2_PIN 2
-#define SERVO3_PIN 4
-#define SERVO4_PIN 5
+#define SERVO_FL_PIN 1
+#define SERVO_RL_PIN 2
+#define SERVO_FR_PIN 4
+#define SERVO_RR_PIN 5
+
+#define DEFAULT_POS 90
+#define WALK_OFFSET 20
+#define WALK_DELAY 150
 
 // Create servo objects
-Servo servo1;
-Servo servo2;
-Servo servo3;
-Servo servo4;
-
+Servo FL; // Front left leg
+Servo FR; // Front right leg
+Servo RL; // Rear left leg
+Servo RR; // Rear right leg
 
 void setup() {
   // Initialize serial communication
   Serial.begin(115200);
 
   // Attach servos to pins
-  servo1.attach(SERVO1_PIN);
-  servo2.attach(SERVO2_PIN);
-  servo3.attach(SERVO3_PIN);
-  servo4.attach(SERVO4_PIN);
+  FL.attach(SERVO_FL_PIN);
+  FR.attach(SERVO_FR_PIN);
+  RL.attach(SERVO_RL_PIN);
+  RR.attach(SERVO_RR_PIN);
 
   // Move servos to initial position (optional)
-  // servo1.write(0);
-  // servo2.write(0);
-  // servo3.write(0);
-  // servo4.write(0);
+  FL.write(DEFAULT_POS);
+  FR.write(DEFAULT_POS);
+  RL.write(DEFAULT_POS);
+  RR.write(DEFAULT_POS);
+
+  delay(3000);
 
 }
 
 void loop() {
-  // Read input from serial monitor
-  if (Serial.available() > 0) {
-    int angle = Serial.parseInt(); // Read the angle value
-
-    // Set servo positions
-    servo1.write(angle);
-    servo2.write(angle);
-    servo3.write(angle);
-    servo4.write(angle);
-
-    Serial.print("Moving servos to ");
-    Serial.print(angle);
-    Serial.println(" degrees");
-
-    // Wait for the servo to reach the desired position
-    delay(1000);
-  }
+    FL.write(DEFAULT_POS - WALK_OFFSET);
+    delay(30);
+    RL.write(DEFAULT_POS + WALK_OFFSET);
+    delay(WALK_DELAY);
+    FR.write(DEFAULT_POS - WALK_OFFSET);
+    delay(30);
+    RR.write(DEFAULT_POS + WALK_OFFSET);
+    delay(WALK_DELAY);
+    FL.write(DEFAULT_POS + WALK_OFFSET);
+    delay(30);
+    RL.write(DEFAULT_POS - WALK_OFFSET);
+    delay(WALK_DELAY);
+    FR.write(DEFAULT_POS + WALK_OFFSET);
+    delay(30);
+    RR.write(DEFAULT_POS - WALK_OFFSET);
+    delay(WALK_DELAY);
 }
