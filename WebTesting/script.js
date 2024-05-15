4f5a290716eaa576b13303270baa6a1f3f978f6d
Floris Boers <floris.boers@hva.nl>
2024-05-15 11:49:20 +0200

    improved on snapping

--- WebTesting/script.js
+++ WebTesting/script.js
@@ -4,6 +4,11 @@
 // let dragOffsetX, dragOffsetY;
 var create_draggable_buttons = Array.from(document.querySelectorAll('.create-draggable-button'));
 var draggable_objects = Array.from(document.querySelectorAll('.draggable'));
+const scale = 20;
+
+function clampToNearestScale(number) {
+  return Math.round(number / scale) * scale;
+}
 
 function loadDraggables(){
     draggable_objects = Array.from(document.querySelectorAll('.draggable'));
@@ -30,8 +35,9 @@ function createDraggableDiv(id, topPosition) {
   // Append the inner div to the outer div
   div.appendChild(innerDiv);
 
-  // Append the outer div to the document body
-  document.body.appendChild(div);
+  parentDiv = document.querySelector("#draggable-container");
+  parentDiv.appendChild(div);
+  
 }
 
 function loadCreateDraggables(){
@@ -53,6 +59,8 @@ loadCreateDraggables();
 const canvas = document.getElementById('draggable-container')
 const CANVAS_WIDTH = 500;
 const CANVAS_HEIGHT = 300;
+
+const NODE_HEIGHT = 50;
 
 const DRAGGABLE_BORDER_X_MIN = 0;
 const DRAGGABLE_BORDER_X_MAX = 500;
@@ -88,67 +96,82 @@ function dragElement(elmnt) {
         console.error("Invalid closestDiv.");
         return;
     }
-      const rect1 = targetDiv;
-      const rect2 = closestDiv.getBoundingClientRect();
+      const rect1 = targetDiv.getBoundingClientRect();
+      const rect2 = closestDiv;
   
       const offsetX = rect2.left - rect1.left;
-      const offsetY = rect2.top -  rect1.top;
+      const offsetY = rect2.top - rect1.top;
       
-      const newX = rect2.top;
-      const newY = rect2.left;
-
+      const newX = rect2.left;
+      let newY = rect2.top;
+      
       targetDiv.style.top = newX + "px";
       targetDiv.style.left = newY + "px";
   }
+
+  function snapToLeftSide(targetDiv){
     
-  
-    function dragMouseDown(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // get the mouse cursor position at startup:
-      pos3 = e.clientX;
-      pos4 = e.clientY;
-      document.onmouseup = closeDragElement;
-      // call a function whenever the cursor moves:
-      document.onmousemove = elementDrag;
+    if (!targetDiv || !targetDiv.getBoundingClientRect) {
+      console.error("Invalid closestDiv.");
+      return;
     }
-  
-    function elementDrag(e) {
-      e = e || window.event;
-      e.preventDefault();
-      // calculate the new cursor position:
-      pos1 = pos3 - e.clientX;
-      pos2 = pos4 - e.clientY;
-      pos3 = e.clientX;
-      pos4 = e.clientY;
+    targetDiv.style.left = "0px";
+  }
+    
+  let newY = 0;
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
 
-      element_width = 180;
-      element_height = 100;
-
-      
-      let newX = (elmnt.offsetTop - pos2);
-      let newY = (elmnt.offsetLeft - pos1);
-      console.log(newX + " " + newY)
-      if(newX < DRAGGABLE_BORDER_Y_MIN || newX >= DRAGGABLE_BORDER_Y_MAX || newY < DRAGGABLE_BORDER_X_MIN || newY >= DRAGGABLE_BORDER_X_MAX) {
-        //console.log("NEE")
-        //closeDragElement();
-
-      } else {
-        elmnt.style.top = newX + "px";
-        elmnt.style.left = newY + "px";
-      }
-
-      //console.log((elmnt.offsetTop - pos2) + "px" + " " + (elmnt.offsetLeft - pos1) + "px");
-      // set the element's new position: 
-    }
-  
     function closeDragElement() {
       // stop moving when mouse button is released:
       document.onmouseup = null;
       document.onmousemove = null;
       var closestDiv = getShortestDistance(elmnt, draggable_objects);
+      newY = clampToNearestScale(newY);
+      elmnt.style.top = newY + "px";  
       console.log(closestDiv);
-      snapToClosestDiv(elmnt, closestDiv);
-
+      // snapToClosestDiv(elmnt, closestDiv);
+      snapToLeftSide(elmnt);
     }
+
+  }
+  
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+
+    element_width = 180;
+    element_height = 100;
+
+    
+    newY = (elmnt.offsetTop - pos2);
+    let newX = (elmnt.offsetLeft - pos1);
+    console.log(newY);
+
+    // console.log(newX + " " + newY);
+    if(newY < DRAGGABLE_BORDER_Y_MIN || newY >= DRAGGABLE_BORDER_Y_MAX || newX < DRAGGABLE_BORDER_X_MIN || newX >= DRAGGABLE_BORDER_X_MAX) {
+      //console.log("NEE")
+      //closeDragElement();
+
+    } else {
+      elmnt.style.top = newY + "px";
+      elmnt.style.left = newX + "px";
+    }
+
+    //console.log((elmnt.offsetTop - pos2) + "px" + " " + (elmnt.offsetLeft - pos1) + "px");
+    // set the element's new position: 
+  }
+  
   }
