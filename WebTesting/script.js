c4e0ccc8891e8e7cc2cebfa702378c8ec5e6ecb1
Floris Boers <floris.boers@hva.nl>
2024-05-24 13:32:10 +0200

    Added refinements and comments in the code

--- WebTesting/script.js
+++ WebTesting/script.js
@@ -10,10 +10,13 @@ const NODE_HEIGHT = 70;
 canvas.style.width = CANVAS_WIDTH + 'px';
 canvas.style.height = CANVAS_HEIGHT + 'px';
 
+//Changeable draggable area, normally set to canvas width and height.
 const DRAGGABLE_BORDER_X_MIN = 0;
 const DRAGGABLE_BORDER_X_MAX = CANVAS_WIDTH;
 const DRAGGABLE_BORDER_Y_MIN = 0;
 const DRAGGABLE_BORDER_Y_MAX = CANVAS_HEIGHT;
+
+// Scale for the finding of the index
 const scale = NODE_HEIGHT ;
 
 var create_draggable_buttons = Array.from(document.querySelectorAll('.create-draggable-button'));
@@ -21,6 +24,7 @@ var draggable_objects = Array.from(document.querySelectorAll('.draggable'));
 var commands_array = {};
 addListNumbers(10);
 
+// Function for adding list numbers to the draggable container
 function addListNumbers(n){
   const list_element = document.querySelector('#list');
   for(var i = 0; i < n; i++){
@@ -32,16 +36,26 @@ function addListNumbers(n){
   }
 }
 
+// Functioin for clamping to the nearest number on the scale, example: on a scale of 20, the number 41 would clamp to 40
 function clampToNearestScale(number) {
   return Math.round(number / scale) * scale;
 }
 
+// Adds draggable functionality to every element with the draggable-parent class
 function loadDraggables(){
   draggable_objects = Array.from(document.querySelectorAll('.draggable-parent'));
   for (let i = 0; i < draggable_objects.length; i++) {
-      let element = draggable_objects[i];
-      dragElement(element);
-      console.log(element);
+    let element = draggable_objects[i];
+    dragElement(element);
+    console.log(element);
+  }
+}
+
+// Adds creation of draggable divs functionality to the create draggable button class
+function loadCreateDraggables(){
+  const create_draggable_buttons = Array.from(document.querySelectorAll('.create-draggable-button'));
+  for (let i = 0; i < create_draggable_buttons.length; i++) {
+    let element = create_draggable_buttons[i];
   }
 }
 
@@ -50,20 +64,28 @@ function createDraggableDiv(id, topPosition, type) {
   var div = document.createElement("div");
   div.id = id;
   div.className = "draggable-parent";
-  div.style.left = "300px";
+  div.style.left = "250px";
   
   // Create the inner div
   var innerDiv = document.createElement("div");
-  
   div.appendChild(innerDiv);
+
+  // Find parent/constainer div
   parentDiv = document.querySelector("#draggable-container");
   parentDiv.appendChild(div);
 
+  // Set it so that when the input is updated, the value is also updated in the commands_array
+  var inputElement = document.createElement("input");
+  inputElement.addEventListener("change", function(e) {
+    let index = findIndexInList(div);
+    setNodeInArray(index, div.id, inputElement.value);
+  })
+
+  // Check what type is set, and add the correct values accordingly
   if(type == 'forward'){
     innerDiv.className = "draggable-button forward";
     innerDiv.textContent = "forward (s):";
     
-    var inputElement = document.createElement("input");
     inputElement.classList.add('input', 'forward-input');
     inputElement.setAttribute('type', 'number');
     div.appendChild(inputElement);
@@ -72,7 +94,6 @@ function createDraggableDiv(id, topPosition, type) {
     innerDiv.className = "draggable-button wait";
     innerDiv.textContent = "wait (s):";
   
-    var inputElement = document.createElement("input");
     inputElement.classList.add('input', 'forward-input');
     inputElement.setAttribute('type', 'number');
     div.appendChild(inputElement);
@@ -81,58 +102,52 @@ function createDraggableDiv(id, topPosition, type) {
     innerDiv.className = "draggable-button turn";
     innerDiv.textContent = "turn (degrees):";
   
-    var inputElement = document.createElement("input");
     inputElement.classList.add('input', 'forward-input');
     inputElement.setAttribute('type', 'number');
     div.appendChild(inputElement);
   }
   else if(type == 'move'){
     innerDiv.className = "draggable-button move";
-    innerDiv.textContent = "move to step (index):";
+    innerDiv.textContent = "move to step:";
   
-    var inputElement = document.createElement("input");
     inputElement.classList.add('input', 'forward-input');
     inputElement.setAttribute('type', 'number');
     div.appendChild(inputElement);
   }
   
 
-  // Append the inner div to the outer div
+  // Reload the draggable divs/add functionality
   loadDraggables();
 }
 
-function loadCreateDraggables(){
-  const create_draggable_buttons = Array.from(document.querySelectorAll('.create-draggable-button'));
-  for (let i = 0; i < create_draggable_buttons.length; i++) {
-    let element = create_draggable_buttons[i];
-  }
-}
-
+// Upload program to robot
 function uploadProgram(){
   console.log(commands_array);
-}
-function reloadNodeValues(){
-  const container = document.querySelector('#draggable-container');
-  const array = container.getElementsByClassName("input");
   
-  for(var i = 0; i < array.length; i++) {
-    console.log(array[i].value);
-  }
 }
 
 loadDraggables();
 loadCreateDraggables();
 
+// Set the correct command into the commands_array
 function setNodeInArray(index, type, value) {
   commands_array[index] = [type, value];
 }
 
+// Finds the index of a draggable block by the height of the element
+function findIndexInList(elmnt){
+  let index = 0;
+  let y = clampToNearestScale(elmnt.offsetTop);
+  index = elmnt.offsetTop / NODE_HEIGHT;
+  return index;
+}
 
-
+// Main function for making a element draggable
 function dragElement(elmnt) {
-  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
-  const moveDiv = elmnt.getElementsByClassName('draggable-button');
-  const inputElement = elmnt.getElementsByClassName('input')[0];
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0; // Init values
+  const moveDiv = elmnt.getElementsByClassName('draggable-button'); // Find the part of the element that when clicked, drags the entire element
+
+  const inputElement = elmnt.getElementsByClassName('input')[0]; // Find the element that holds the input value
   moveDiv[0].onmousedown = dragMouseDown;
 
   function snapToLeftSide(targetDiv){
@@ -150,27 +165,20 @@ function dragElement(elmnt) {
     // get the mouse cursor position at startup:
     pos3 = e.clientX;
     pos4 = e.clientY;
-    document.onmouseup = closeDragElement;
-    // call a function whenever the cursor moves:
-    document.onmousemove = elementDrag;
-
+    document.onmouseup = closeDragElement; // call a function when the mouse is let go
+    document.onmousemove = elementDrag; // call a function whenever the cursor moves:
 
     function closeDragElement() {
       // stop moving when mouse button is released:
       document.onmouseup = null;
       document.onmousemove = null;
-      newY = clampToNearestScale(newY);
-         elmnt.style.top = newY + "px";  
-      // snapToClosestDiv(elmnt, closestDiv);
-      console.log(elmnt.id);
-      const index = newY / NODE_HEIGHT;
+      newY = clampToNearestScale(newY); // clamp to nearest value on the scale, this adds the snapping functionality
+      elmnt.style.top = newY + "px";  // Set according height
+      const index = newY / NODE_HEIGHT; 
       const input = inputElement.value;
-      console.log(index);
-      setNodeInArray(index, elmnt.id, input);
-      // reloadNodeValues();
-      snapToLeftSide(elmnt);
+      setNodeInArray(index, elmnt.id, input); // Set command same as the block in the commands_array
+      snapToLeftSide(elmnt); // Snaps the element to the left side
     }
-
   }
   
   function elementDrag(e) {
@@ -185,25 +193,14 @@ function dragElement(elmnt) {
     element_width = 180;
     element_height = 100;
     
+    newY = (elmnt.offsetTop - pos2); // Calculate new Y position
+    let newX = (elmnt.offsetLeft - pos1); // Calculate new X position
 
-    
-    newY = (elmnt.offsetTop - pos2);
-    let newX = (elmnt.offsetLeft - pos1);
-
-    // console.log(newX + " " + newY);
+    // Calculate if the new position fits inside the draggable area
     if(newY < DRAGGABLE_BORDER_Y_MIN || newY >= DRAGGABLE_BORDER_Y_MAX || newX < DRAGGABLE_BORDER_X_MIN || newX >= DRAGGABLE_BORDER_X_MAX) {
-      //console.log("NEE")
-      //closeDragElement();
-
     } else {
-      // elmnt.style.top = newY + "px";
       elmnt.style.top = newY + "px";
       elmnt.style.left = newX + "px";
-      
     }
-
-    //console.log((elmnt.offsetTop - pos2) + "px" + " " + (elmnt.offsetLeft - pos1) + "px");
-    // set the element's new position: 
   }
-  
-  }
+}
