b0cf2e9627adc47249bf8373bd3efed2ebfcfca6
Timo <timo.lambregts@hva.nl>
2024-05-30 10:36:24 +0200

    codeblock got it's colour back

--- WebTesting/script.js
+++ WebTesting/script.js
@@ -115,7 +115,6 @@ function createDraggableDiv(id, topPosition, type) {
     div.appendChild(inputElement);
   }
 
-<<<<<<< HEAD
   else if (type == 'animation') {
     innerDiv.className = "draggable-button animation";
     innerDiv.textContent = "do animation:";
@@ -145,9 +144,6 @@ function createDraggableDiv(id, topPosition, type) {
   }
 
 
-=======
-  // Reload the draggable divs/add functionality
->>>>>>> 1f53ccd559ac56f81b781ad3dc6d070006626742
   loadDraggables();
 }
 
