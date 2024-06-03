2a011a6bc2bf9120f22d230aad43f60e42b4224c
Ilias <Iliastakassa@hotmail.com>
2024-06-03 14:25:19 +0200

    updated rightward command js

--- web/static/javascript/buttons.js
+++ web/static/javascript/buttons.js
@@ -1,5 +1,6 @@
 const forwardButton = document.querySelector('.arrow-btn.arrow-up');
-const leftward = document.querySelector('.arrow-btn.arrow-left'); 
+const leftwardButton = document.querySelector('.arrow-btn.arrow-left'); 
+const rightwardButton = document.querySelector('.arrow-btn.arrow-right'); 
 
 forwardButton.addEventListener('mousedown', () => {
     forwardButton.style.transform = 'translateY(2px)';
@@ -38,9 +39,9 @@ forwardButton.addEventListener('mouseup', () => {
 
 // -------------------------------------
 
-leftward.addEventListener('mousedown', () => {
-    forwardButton.style.transform = 'translateY(2px)';
-    forwardButton.style.boxShadow = '0px 2px 0px #888';
+leftwardButton.addEventListener('mousedown', () => {
+    leftwardButton.style.transform = 'translateY(2px)';
+    leftwardButton.style.boxShadow = '0px 2px 0px #888';
 
     const data = {
         task: 'leftward' 
@@ -69,6 +70,43 @@ leftward.addEventListener('mousedown', () => {
 });
 
 leftward.addEventListener('mouseup', () => {
-    forwardButton.style.transform = '';
-    forwardButton.style.boxShadow = '';
+    leftwardButton.style.transform = '';
+    leftwardButton.style.boxShadow = '';
+});
+
+// ---------------------------------
+
+rightwardButton.addEventListener('mousedown', () => {
+    rightwardButton.style.transform = 'translateY(2px)';
+    rightwardButton.style.boxShadow = '0px 2px 0px #888';
+
+    const data = {
+        task: 'rightward' 
+    };
+
+    // Verstuur het JSON-bericht naar de API
+    fetch('http://Hier_eigen_IP/api.php?action=task', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('API response:', data);
+    })
+    .catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+});
+
+leftward.addEventListener('mouseup', () => {
+    rightwardButton.style.transform = '';
+    rightwardButton.style.boxShadow = '';
 });
