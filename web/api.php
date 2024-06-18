4afdb0ce54c2a1686b5d6ddfad7fc542e77cc67c
Ilias <Iliastakassa@hotmail.com>
2024-06-18 21:30:24 +0200

    added comments to api.php

--- web/api.php
+++ web/api.php
@@ -1,27 +1,33 @@
 <?php
+// Require external scripts for sending commands and getting connections
 require 'send_command.php';
 require 'get_connections.php';
 
-// Allow CORS
+// Allow CORS (Cross-Origin Resource Sharing) and set the content-type header to JSON
 header('Content-Type: application/json');
 header('Access-Control-Allow-Origin: *');
 
+// Retrieve the HTTP request method and the action parameter from the URL (if present)
 $method = $_SERVER['REQUEST_METHOD'];
 $action = isset($_GET['action']) ? $_GET['action'] : '';
 
+// Function to send data using the sendCommand function from send_command.php
 function send($data){
     sendCommand($data);
 }
 
+// Function to get connections using the getConnections function from get_connections.php
 function connections(){
     getConnections();
 }
 
+// Function to validate input and forward it to the send function
 function validateInput(){ 
-    $data = file_get_contents('php://input');
-    send($data);
+    $data = file_get_contents('php://input'); // Get raw POST data
+    send($data); // Forward the retrieved data
 }
 
+// Main function to handle the incoming request
 function handleRequest() {
     // Retrieve global variables $method and $action
     global $method, $action;
@@ -40,14 +46,13 @@ function handleRequest() {
             break;
         case 'GET':
             if ($action == 'connections') {
-                // Call function to handle submission of new data
+                // Call function to handle retrieval of connections
                 connections();
             } else {
-                // Respond with an error message if action is unknown for POST requests
-                echo json_encode(["error" => "Unknown action for get"]);
+                // Respond with an error message if action is unknown for GET requests
+                echo json_encode(["error" => "Unknown action for GET"]);
             }
             break;
-
         default:
             // Respond with an error message if request method is unsupported
             echo json_encode(["error" => "Unsupported request method"]);
@@ -55,5 +60,7 @@ function handleRequest() {
     }
 }
 
+// Call the main function to handle the request
 handleRequest();
 ?>
+
