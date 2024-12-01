946ecf44e5130edd2366723bc3a27572e2ef2ff9
Elizaveta <elizaveta.sabanova@hva.nl>
2024-12-01 17:59:49 +0100

    Implemented controlled fall to the right for robot

---
+++ embedded/EndianB-Robot dog/main/fall_right.h
@@ -0,0 +1,17 @@
+#ifndef FALL_RIGHT_H
+#define FALL_RIGHT_H
+
+#include <ESP32Servo.h>
+
+const int fallRightPositions[4] = {0, 0, 180, 180}; // FL, FR, RL, RR
+
+void fallRight(Servo& FL, Servo& FR, Servo& RL, Servo& RR) {
+    Serial.println("Falling to the right");
+    FL.write(fallRightPositions[0]);
+    FR.write(fallRightPositions[1]);
+    RL.write(fallRightPositions[2]);
+    RR.write(fallRightPositions[3]);
+}
+
+#endif
+
