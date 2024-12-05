ee4872926a86cace204c0116e7d2f8f3897c181f
Tom Jonker <tjj.jonker@gmail.com>
2024-12-05 15:20:55 +0100

    Added Fall cpp file

---
+++ embedded/EndianB-Robot dog/main/Fall.cpp
@@ -0,0 +1,15 @@
+#include "Fall.h"
+
+void Fall::FallRight(Servo& FL, Servo& FR, Servo& RL, Servo& RR){
+    FL.write(0);
+    FR.write(0);
+    RL.write(MAX_LEG_ROTATION);
+    RR.write(MAX_LEG_ROTATION);
+}
+
+void Fall::FallLeft(Servo& FL, Servo& FR, Servo& RL, Servo& RR){
+    FL.write(MAX_LEG_ROTATION);
+    FR.write(MAX_LEG_ROTATION);
+    RL.write(0);
+    RR.write(0);
+}
