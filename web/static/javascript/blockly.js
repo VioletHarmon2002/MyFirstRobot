c6bd7466ab5190364af89e2cf00417e7a8429cb6
Tim van Dantzig - Personal laptop <tim.van.dantzig@hva.nl>
2024-12-06 11:29:37 +0100

    CHANGE js scripts
    added localhost as the connection ip, server is running locally for testing

--- web/static/javascript/blockly.js
+++ web/static/javascript/blockly.js
@@ -2,7 +2,7 @@ const HVA_DARK_COLOR = '#28147C'
 const HVA_LIGHT_COLOR = '#381CB4'
 const forward_color = "#1b97b3";
 const turn_color = "#16c548";
-const IP_ADRESS = '192.168.137.103';
+const IP_ADRESS = '127.0.0.1';
 
 let running;
 
